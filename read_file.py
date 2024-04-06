ba9314305593293ec2680c15f74d4a7ab7d77675
Ruslan Bektashev <ruslanbektashev@gmail.com>
2024-04-07 00:04:06 +0300

    prepare for integration

--- read_file.py
+++ read_file.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 def mt_code_read(path: str) -> tuple:
     program = {}
-    logs = ''
     file_type = path.split('.')[-1]
     if file_type == 'xlsx':
         data = pd.read_excel(path)
@@ -12,56 +11,41 @@ def mt_code_read(path: str) -> tuple:
     else:
         logs = 'Неправильный формат файла. Поддерживаемые форматы: .xlsx/.csv'
         return program, logs
-
     try:
         df = pd.DataFrame(data, columns=['A', 'Q', 'φ', 'ψ', 'H'])
         if df.isnull().values.any():
-            logs = ('Неправильно заполнена таблица, либо пустые значения в' +
-                    ' ячейках. Заполняйте строго по шаблону, прикрепленному к' +
-                    ' разделу на контесте'
-                    )
+            logs = ('Неправильно заполнена таблица, либо пустые значения в '
+                    'ячейках. Заполняйте строго по шаблону, прикрепленному к '
+                    'разделу на контесте')
             return program, logs
-
         for line in df.iloc:
             if line['Q'] not in program:
                 program[line['Q']] = [(0, 'S', line['Q']), (1, 'S', line['Q'])]
-
             program[line['Q']][int(line['A'])] = (int(line['ψ']), line['H'].upper(), line['φ'])
             if line['φ'] not in program:
                 program[line['φ']] = [(0, 'S', line['φ']), (1, 'S', line['φ'])]
-
         logs = 'Файл успешно считан\n'
-
         move_alphabet = set(df['H'].values.tolist())
         if abs(max(df['A'].values.tolist())) > 1 or abs(max(df['ψ'].values.tolist())) > 1:
-            logs = ('Входной и выходной алфавиты Машины Тьюринга' +
-                    ' могут состоять только из 0 и 1'
-                    )
+            logs = 'Входной и выходной алфавиты Машины Тьюринга  могут состоять только из 0 и 1'
             program = {}
-
         elif move_alphabet.union({'L', 'R', 'S'}) != {'L', 'R', 'S'}:
-            logs = ('Для перемещения по ленте можно использовать только' +
-                    ' нижеследующие символы:' +
-                    '\nL - налево\nR - направо\nS - остановка'
-                    )
+            logs = ('Для перемещения по ленте можно использовать только нижеследующие символы:\n'
+                    'L - налево\n'
+                    'R - направо\n'
+                    'S - остановка')
             program = {}
-
     except Exception as e:
         if 'invalid literal for int() with base 10:' in str(e):
-            logs = 'Cпутаны столбцы состояний и значений Машины Тьюринга'
-
+            logs = 'Перепутаны столбцы состояний и значений Машины Тьюринга'
         elif "'int' object has no attribute 'upper'" in str(e):
-            logs = ('Для перемещения по ленте можно использовать только' +
-                    ' нижеследующие символы:' +
-                    '\nL - налево\nR - направо\nS - остановка'
-                    )
-
+            logs = ('Для перемещения по ленте можно использовать только нижеследующие символы:\n'
+                    'L - налево\n'
+                    'R - направо\n'
+                    'S - остановка')
         elif 'max() arg is an empty sequence' in str(e):
             logs = 'Подана пустая таблица'
-
         else:
             logs = str(e)
-
         program = {}
-
     return program, logs
--- turing_machine.py
+++ turing_machine.py
@@ -1,8 +1,12 @@
-import time
 import re
-from math import log
+import time
+
 from random import randint
+from typing import Union
+
 from read_file import mt_code_read
+
+RE_VAR_PATTERN = re.compile(r"x[0-9]+")
 
 
 class TuringMachine:
@@ -10,11 +14,7 @@ class TuringMachine:
         self.program = None
         self.tape = None
         self.start_state = None
-        self.movement_encode = {
-                               'R': +1,
-                               'L': -1,
-                               'S': 0
-                               }
+        self.movement_encode = {'R': +1, 'L': -1, 'S': 0}
         self.variables_num = 0
         self.function = None
         self.variables_names = None
@@ -22,109 +22,78 @@ class TuringMachine:
         self.start_time = 0
         self.logs = ''
 
-    def test(self, submission_file: str, problem: str, criteria: list[int],
-             time_limit: int, test: list[int] | int) -> tuple:
+    def test(self, submission_file: str, problem: str, criteria: list[int], time_limit: int,
+             test: Union[list[int], int]) -> tuple:
         self.program, self.logs = mt_code_read(submission_file)
         if len(self.program) == 0:
             return 2, self.logs
-
         self.start_state = list(self.program.keys())[0]
         self.function = problem.replace('div', '//').replace('mod', '%')
         self.function = self.function.replace('^', '**')
-        variables_in_problem = re.findall("x[0-9]+", problem)
+        variables_in_problem = re.findall(RE_VAR_PATTERN, problem)
         self.variables_names = sorted(list(set(variables_in_problem)))
         self.variables_num = len(self.variables_names)
         self.time_limit = time_limit
-
         if type(test) is int:
             correct_answers = 0
             log_flag = ('log' in self.function)
-            mark_multiplier = 100/test
+            mark_multiplier = 100 / test
             self.start_time = time.time()
-            
             for x in range(0 + log_flag, test + log_flag):
-                values = [abs(x + randint(-5, 5)) + log_flag for y in range(self.variables_num)]
+                values = [abs(x + randint(-5, 5)) + log_flag for _ in range(self.variables_num)]
                 if self.variables_num > 0:
                     values[0] = x
-
-                mt_tape = '0'.join(j for j in [''.join(str(1) for n in range(j+1)) for j in values])
-
-                result_tape = self.run(list(mt_tape))
-                if result_tape is None:
+                mt_tape = '0'.join(j for j in [''.join(str(1) for _ in range(j + 1)) for j in values])
+                ok = self.run(list(mt_tape))
+                if not ok:
                     break
-
-                MT_value = sum([int(j) for j in result_tape])-1
+                MT_value = sum([int(j) for j in self.tape]) - 1
                 func_value = self.calculate(values)
-
                 if MT_value != func_value:
-                    self.logs += (f'Ошибка! Значение функции: {func_value},' +
-                                  f' Значение Машины Тьюринга: {MT_value},' +
-                                  f' Значения переменных: {values}\n'
-                                  )
-
+                    self.logs += (f'Ошибка! Значение функции: {func_value}, Значение Машины Тьюринга: {MT_value}, '
+                                  f'Значения переменных: {values}\n')
                 else:
-                    self.logs += (f'Успешно! Значение функции: {func_value},' +
-                                  f' Значение Машины Тьюринга: {MT_value},' +
-                                  f' Значения переменных: {values}\n'
-                                  )
+                    self.logs += (f'Успешно! Значение функции: {func_value}, Значение Машины Тьюринга: {MT_value}, '
+                                  f'Значения переменных: {values}\n')
                     correct_answers += 1
-
             self.logs += f'Количество правильных ответов: {correct_answers}'
             mark = self.get_mark(correct_answers, criteria, mark_multiplier)
-
             return mark, self.logs
-
         else:
             if len(test) != self.variables_num:
-                raise Exception('Количество переменных не совпадает с' +
-                                ' количеством поданных значений. Переменные' +
-                                ' необходимо называть: x1, x2, ..., xn'
-                                )
-
-            mt_tape = '0'.join(j for j in [''.join(str(1) for n in range(j+1)) for j in test])
-
-            result_tape = self.run(list(mt_tape))
-            MT_value = sum([int(j) for j in result_tape])-1
+                raise Exception('Количество переменных не совпадает с количеством поданных значений. Переменные '
+                                'необходимо называть: x1, x2, ..., xn')
+            mt_tape = '0'.join(j for j in [''.join(str(1) for _ in range(j + 1)) for j in test])
+            self.run(list(mt_tape))
+            MT_value = sum([int(j) for j in self.tape]) - 1
             func_value = self.calculate(test)
             mark = {True: 5, False: 2}[MT_value == func_value]
             if mark == 2:
-                self.logs += (f'Ошибка! Значение функции: {func_value},' +
-                              f' Значение Машины Тьюринга: {MT_value}'
-                              )
-
+                self.logs += f'Ошибка! Значение функции: {func_value}, Значение Машины Тьюринга: {MT_value}\n'
             else:
-                self.logs += (f'Успешно! Значение функции: {func_value},' +
-                              f' Значение Машины Тьюринга: {MT_value}'
-                              )
-
+                self.logs += f'Успешно! Значение функции: {func_value}, Значение Машины Тьюринга: {MT_value}\n'
             return mark, self.logs
 
     def calculate(self, values: list[int]) -> int:
         func = self.function
         for var in range(self.variables_num):
             func = func.replace(self.variables_names[var], str(values[var]))
-
         try:
             func_value = int(eval(func))
-
         except Exception:
-            raise Exception('Неправильная запись функции.\n' +
-                            'Переменные следует называть: x1, x2, ..., xn\n' +
-                            'Умножение: x1*4 / x1*x2\n' +
-                            'Возведение в степень: x1^2 / x1^x2\n' +
-                            'Сложение: x1+2 / x1+x2\n' +
-                            'Остаток от деления: x1mod7 / x1modx2\n' +
-                            'Деление без остатка: x1div3 / x1divx2\n' +
-                            'Логарифм: log(x1, 2) / log(x1, x2).' +
-                            ' Основание логарифма - второй аргумент'
-                            )
-
+            raise Exception('Неправильная запись функции.\n'
+                            'Переменные следует называть: x1, x2, ..., xn\n'
+                            'Умножение: x1*4 / x1*x2\n'
+                            'Возведение в степень: x1^2 / x1^x2\n'
+                            'Сложение: x1+2 / x1+x2\n'
+                            'Остаток от деления: x1mod7 / x1modx2\n'
+                            'Деление без остатка: x1div3 / x1divx2\n'
+                            'Логарифм: log(x1, 2) / log(x1, x2). '
+                            'Основание логарифма - второй аргумент')
         return func_value
 
-    def get_mark(self, correct_answers: int, criteria: list[int],
-                 mark_multiplier: int) -> int:
-        score = correct_answers*mark_multiplier
-
+    def get_mark(self, correct_answers: int, criteria: list[int], mark_multiplier: float) -> int:
+        score = correct_answers * mark_multiplier
         if score >= criteria[0]:
             return 5
         elif score >= criteria[1]:
@@ -134,44 +103,36 @@ class TuringMachine:
         else:
             return 2
 
-    def run(self, tape: list) -> list:
+    def run(self, tape: list) -> bool:
         self.tape = tape
         change_to = 0
         move_to = 1
         new_state = 2
         current_index = 0
         current_state = self.start_state
-
         while True:
-            if (current_index >= len(self.tape) or current_index < 0):
+            if current_index >= len(self.tape) or current_index < 0:
                 current_index = self.tape_builder(current_index)
-
             rows = self.program[current_state]
             current_row = rows[int(self.tape[current_index])]
             prev = self.tape[current_index]
             self.tape[current_index] = str(current_row[change_to])
-
-            if (not self.movement_encode[current_row[move_to]] and
-                    current_state == current_row[new_state] and
-                    prev == str(current_row[change_to])):
+            if (not self.movement_encode[current_row[move_to]]
+                    and current_state == current_row[new_state]
+                    and prev == str(current_row[change_to])):
                 break
-
             current_index += self.movement_encode[current_row[move_to]]
             current_state = current_row[new_state]
-
             working_time = time.time() - self.start_time
             if working_time > self.time_limit:
                 self.logs += 'Превышено время работы программы\n'
-                return None
-
-        return self.tape
+                return False
+        return True
 
     def tape_builder(self, current_index: int) -> int:
         while current_index >= len(self.tape):
             self.tape += '0'
-
         while current_index < 0:
             self.tape = ['0'] + self.tape
             current_index += 1
-
         return current_index
