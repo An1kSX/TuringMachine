2404c781751715e7210fca9c723a0f730a6dc63e
An1kSX <70918749+An1kSX@users.noreply.github.com>
2025-04-25 00:18:20 +0500

    Add files via upload

--- native/cpp/read_file.cpp
+++ native/cpp/read_file.cpp
@@ -64,8 +64,8 @@ std::pair<Program, std::string> mt_code_read(const std::string& path) {
         std::string col = header[i];
         if (col == "A") idxA = i;
         else if (col == "Q") idxQ = i;
-        else if (col == "phi") idxPhi = i;
-        else if (col == "psi") idxPsi = i;
+        else if (col == "φ") idxPhi = i;
+        else if (col == "ψ") idxPsi = i;
         else if (col == "H") idxH = i;
     }
     if (idxA == -1 || idxQ == -1 || idxPhi == -1 || idxPsi == -1 || idxH == -1) {
