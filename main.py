9c0de421f6f5f78351269cd6e76ee8eacfa7b4ab
An1kSX <70918749+An1kSX@users.noreply.github.com>
2025-04-24 22:54:25 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ mark, logs = mt.test(
 	problem="5*(x1 + 3)^2",
 	criteria=[85, 65, 55],
 	time_limit=30,
-	launch_args=42
+	launch_args=50
 	)
 
 print(f"{logs}\nОценка: {mark}")
--- tm_wrapper.py
+++ tm_wrapper.py
@@ -7,23 +7,21 @@ import ctypes
 
 class TuringMachine:
 	@staticmethod
-	def _xlsx_to_csv(xlsx_path: Path) -> Path:
+	def _xlsx_to_csv(xlsx_path):
 		csv_path = xlsx_path.with_suffix('.csv')
 		df = pd.read_excel(xlsx_path, sheet_name=0, header=None)
 		df.to_csv(csv_path, index=False, header=False, sep=';', encoding="utf-8-sig")
 
-		xlsx_path.unlink()
-
 		return csv_path
 
-	def test(self, submission_file: str, problem: str, criteria: list[int], time_limit: int = 30, launch_args: int = 1):
+	def test(self, submission_file, problem, criteria, time_limit = 30, launch_args = 1):
 		path = Path(submission_file)
 		if path.suffix.lower() in {'.xlsx', '.xls'}:
 			path = self._xlsx_to_csv(path)
 
-		criteria_str = ",".join(map(str, criteria)).encode("utf‑8")
-		path = str(path).encode("utf‑8")
-		problem = problem.encode("utf‑8")
+		criteria_str = ",".join(map(str, criteria)).encode("utf-8")
+		path = str(path).encode("utf-8")
+		problem = problem.encode("utf-8")
 		log_buf = ctypes.create_string_buffer(16 * 1024)
 
 		mark = tm_test(path, problem, criteria_str, time_limit, launch_args, log_buf, ctypes.sizeof(log_buf))
