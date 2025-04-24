c62e6a813f5cab7d5997cc36d019cd3f77754ae5
An1kSX <70918749+An1kSX@users.noreply.github.com>
2025-04-24 22:56:25 +0500

    Add files via upload

--- tm_wrapper.py
+++ tm_wrapper.py
@@ -14,7 +14,7 @@ class TuringMachine:
 
 		return csv_path
 
-	def test(self, submission_file, problem, criteria, time_limit = 30, launch_args = 1):
+	def test(self, submission_file, problem, criteria, time_limit = 30, launch_args = 1, observer=None):
 		path = Path(submission_file)
 		if path.suffix.lower() in {'.xlsx', '.xls'}:
 			path = self._xlsx_to_csv(path)
